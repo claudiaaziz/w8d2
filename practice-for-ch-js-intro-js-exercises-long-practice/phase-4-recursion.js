2cacc644af08a599fc8a714abac1429e517c5883
claudiaaziz <claudiahannahh@gmail.com>
2023-10-10 17:31:27 -0400

    finished sumrec

--- practice-for-ch-js-intro-js-exercises-long-practice/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice/phase-4-recursion.js
@@ -1,1 +1,23 @@
 // Your code here
+
+const range = (start, end) => {
+    const result = []
+
+    if (start > end) return []
+    result.push(start)
+    return result.concat(range(start + 1, end))
+}
+
+// console.log(range(1,6))
+
+const sumRec = arr => {
+    let sum = arr[0]
+    if (arr.length < 1) return 0
+    return sum += sumRec(arr.slice(1))
+}
+
+// console.log(sumRec([3,5,6]))
+
+const exponenet = (base, exp) => {
+    
+}

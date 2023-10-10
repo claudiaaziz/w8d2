3b6902f43695c86cbcf56ebc457dffbd60029356
claudiaaziz <claudiahannahh@gmail.com>
2023-10-10 17:06:39 -0400

    finished phase 3

--- practice-for-ch-js-intro-js-exercises-long-practice/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice/phase-3-iteration.js
@@ -6,14 +6,30 @@ Array.prototype.bubbleSort = function() {
     while (!sorted) {
         sorted = true;
         for (let i = 0; i < this.length - 1; i++) {
-            if (this[i] > this[i + 1]) [this[i + 1], this[i]] = [this[i], this[i + 1]];
-            sorted = false;
+            if (this[i] > this[i + 1]) {
+                [this[i + 1], this[i]] = [this[i], this[i + 1]];
+                sorted = false;
+            }
         }
     }
 
     return this
 };
 
-arr = [3,5,6,7,5]
+// arr = [3,5,6,7,5]
 
-arr.bubbleSort()
+// console.log(arr.bubbleSort()) 
+
+String.prototype.substrings = function() {
+    const subs = []
+
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i+1; j < this.length; j++) {
+            subs.push(this.slice(i,j))
+        }
+    }
+
+    return subs
+}
+
+console.log("claudia".substrings())
